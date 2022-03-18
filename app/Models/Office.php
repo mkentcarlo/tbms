72777c12a2b23a723309103e4b0ca154d71c64b6
Kent <kent@buddyboss.com>
2022-03-18 14:52:40 +0800

    fixed

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -21,6 +21,6 @@ class Office extends Model
 
     public function getAppropriation($year)
     {
-        return Allotment::where('year', $year)->where('month', '0')->sum('amount')->where('office_id', $this->id);
+        return Allotment::where('year', $year)->where('month', '0')->where('office_id', $this->id)->sum('amount');
     }
 }
