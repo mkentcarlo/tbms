500cd0385a2a66bb46085b940f616eb935abaf08
Kent <kent@buddyboss.com>
2022-06-30 18:00:50 +0800

    reports_export

--- resources/views/dashboard/reports/export.blade.php
+++ resources/views/dashboard/reports/export.blade.php
@@ -4,7 +4,7 @@
         foreach($exportdata as $data): ?>
             <tr>
                 <?php foreach($data as $d): ?>
-                    <td><?php echo 'ddf'; ?></td> 
+                    <td><?php echo str_replace('/', '', $d); ?></td> 
                 <?php endforeach; ?>
             </tr>
         <?php endforeach; ?>
