1775a4d87af9416acdf94d63a55f963f0d0192d9
Kent <kent@buddyboss.com>
2022-06-30 18:06:57 +0800

    reports_export

--- resources/views/dashboard/reports/export.blade.php
+++ resources/views/dashboard/reports/export.blade.php
@@ -2,13 +2,10 @@
     <tbody>
         <?php 
         foreach($exportdata as $k => $data): 
-        if($k > 30){
-            continue;
-        }
         ?>
             <tr>
                 <?php foreach($data as $d): ?>
-                    <td><?php echo str_replace('/', '', $d); ?></td> 
+                    <td><?php echo stripslashes($id); ?></td> 
                 <?php endforeach; ?>
             </tr>
         <?php endforeach; ?>
