44eedf97fb9a458a2d1a0b3e29b65d7e1e1b8bd8
Kent <kent@buddyboss.com>
2022-08-16 18:13:57 +0800

    Fixed appropriation

--- resources/views/dashboard/allotments/create.blade.php
+++ resources/views/dashboard/allotments/create.blade.php
@@ -151,7 +151,7 @@
 
      $("#frmCreateAllotment").submit(function(e){
       var balance = parseInt($("#appropriation_balance").val());
-      if(balance < 0 && $('#year').val() == ''){
+      if(balance < 0 && $('#month').val() == ''){
         e.preventDefault();
         alert('Insufficient appropriation balance.');
       }
